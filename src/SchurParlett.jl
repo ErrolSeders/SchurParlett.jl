01815c9247c59e7a7cebcbd62b7c8baea82de95f
Errol Seders <errolseders@gmail.com>
2025-06-04 14:54:03 +0200

    Reorganized old code into package

--- src/SchurParlett.jl
+++ src/SchurParlett.jl
@@ -1,5 +1,119 @@
 module SchurParlett
 
-# Write your package code here.
+include("blocking.jl")
+include("unionfind.jl")
+
+using TaylorSeries
+using UnicodePlots
+using LinearAlgebra
+using DelaunayTriangulation
+
+#!TODO!:
+# 1:    Add condition number calculation
+# 2:    Try and trim Delaunay blockings allocations size down.
+#       Doing so should make it faster than the other algorithm
+#       for sufficiently large matrices.
+# 3:    Investigate more accurate and fast ways to do
+#       Taylor Series and Sylvester solving.
+
+function parlett_recurrance(f, T::UpperTriangular)
+    n = size(T)[1]
+    F = zeros(ComplexF64, (n, n))
+    for i ∈ 1:n
+        F[i, i] = f(T[i, i])
+    end
+    for j ∈ 2:n
+        for i in j-1:-1:1
+            denom = T[i, i] - T[j, j]
+            S = (F[i, i] - F[j, j]) / denom
+            for k ∈ i+1:j-1
+                S += (F[i, k] * T[k, j] - T[i, k] * F[k, j]) / denom
+            end
+            F[i, j] = T[i, j] * S
+        end
+    end
+    return F
+end
+
+function block_parlett_recurrance(f, T, blocks)
+    F = zeros(ComplexF64, size(T))
+    for (i, block) in enumerate(blocks)
+        Tii = T[block, block]
+        n = size(Tii)[1]
+        if n == 1
+            F[block, block] = f.(Tii)
+        else
+            σ = tr(Tii) / n
+            M = Tii - σ * I(n)
+            taylor = taylor_expand(f, σ, order=10)
+            F[block, block] = taylor(M)
+        end
+        n = length(blocks)
+        for i ∈ 1:n
+            for j ∈ i+1:n
+                bi, bj = blocks[i], blocks[j]
+                C = F[bi, bi] * T[bi, bj] - T[bi, bj] * F[bj, bj]
+                for k ∈ i+1:j-1
+                    bk = blocks[k]
+                    C += F[bi, bk] * T[bk, bj] - T[bi, bk] * F[bk, bj]
+                end
+                A = T[bi, bi]
+                B = T[bj, bj]
+                # Is there a faster way to calculate this?
+                # Sylvester does schur decompositions on A and B!
+                # This can also fail for some matrices with a very cryptic
+                # LAPACKException(1)
+                F[bi, bj] .= sylvester(A, -B, -C)
+            end
+        end
+    end
+    return F
+end
+
+"""
+  `schur_parlett(f, A [, ε=0.1])`
+
+  For given *complex valued* function `f: C -> C` and complex matrix `A`
+  compute the matrix function `f(A)` using the Schur-Parlett algorithm
+
+  `ε > 0` is a blocking parameter. This method is highly sensitive to the
+  clustering and distribution of eigenvalues, *tuning this value is important*.
+
+"""
+function schur_parlett(f, A::Matrix{<:Complex}, δ=0.1)::AbstractMatrix
+    @assert δ > 0 "Blocking parameter `ε` cannot be negative!"
+    Q = schur(A) #! Consider writing a mutating version that uses schur!(A) for potential 2x speedup.
+    T, Z, Λ = Q
+    if isdiag(T)
+        F = f.(T)
+        return Z * F * Z'
+    else
+        T = T |> UpperTriangular
+    end
+
+    if all(z -> norm(z.im) <= Float64 |> eps |> sqrt, Λ) # All imaginary components ≈ 0
+        L = map(z -> z.re, Λ)
+        q = real_eigenvalue_blocking(δ, L)
+    else
+        q = block_pattern(δ, Λ)
+    end
+
+    perm = collect(1:length(q))
+    for c ∈ reverse(1:length(q))
+        select = (q .== c)
+
+        selected = findall(select)
+        remaining = findall(.!select)
+        reorder = [selected; remaining]
+        ordschur!(Q, select)
+        perm = perm[reorder]
+    end
+    T, Z, Λ = Q
+    q_reorder = q[perm]
+    blocks = block_indices(q_reorder)
+    F = block_parlett_recurrance(f, T, blocks)
+    Z * F * Z'
+end
+
 
 end
---
+++ src/blocking.jl
@@ -0,0 +1,113 @@
+struct UnionFind
+    parent::Vector{Int}
+    size::Vector{Int}
+end
+
+function UnionFind(n)
+    UnionFind(collect(1:n), ones(Int, n))
+end
+
+function find(uf::UnionFind, x::Int)
+    while uf.parent[x] != x
+        uf.parent[x] = uf.parent[uf.parent[x]] # Path compression
+        x = uf.parent[x]
+    end
+    return x
+end
+
+function union!(uf::UnionFind, x::Int, y::Int)
+    xroot = find(uf, x)
+    yroot = find(uf, y)
+    if xroot == yroot
+        return
+    end
+    # Union by size
+    if uf.size[xroot] < uf.size[yroot]
+        uf.parent[xroot] = yroot
+        uf.size[yroot] += uf.size[xroot]
+    else
+        uf.parent[yroot] = xroot
+        uf.size[xroot] += uf.size[yroot]
+    end
+end
+
+function labels_from_uf(uf::UnionFind)
+    n = length(uf.parent)
+    labels = zeros(Int, n)
+    label = 0
+    root_to_label = Dict{Int,Int}()
+    for i in 1:n
+        root = find(uf, i)
+        if !haskey(root_to_label, root)
+            label += 1
+            root_to_label[root] = label
+        end
+        labels[i] = root_to_label[root]
+    end
+    return labels
+end
+
+function real_eigenvalue_blocking(δ::AbstractFloat, Λ::Vector{<:Real})
+    sorted, perm = sort(Λ), sortperm(Λ)
+    gaps = [Inf; diff(sorted)]
+    cluster_ids = cumsum(gaps .> δ)
+
+    invperm = invpermute!([1:(sorted|>length)...], perm)
+    return cluster_ids[invperm]
+end
+
+# This function works as expected, but is ≈5x slower than block_pattern for matrices of a reasonable size sadly.
+function delaunay_graph_blocking(δ::AbstractFloat, Λ::Vector)
+    λp = map(z -> (z.re, z.im), Λ)
+    n = λp |> length
+    uf = UnionFind(n)
+    for (i, j) ∈ (triangulate(λp) |> get_graph).edges
+        if i > 0 && j > 0 && abs(Λ[i] - Λ[j]) <= δ
+            union!(uf, i, j)
+        end
+    end
+    return labels_from_uf(uf)
+end
+
+"""
+  `block_pattern(ε::AbstractFloat, T::UpperTriangular, Λ::Vector{Complex})`
+
+  Reorder and block the upper triangular schur factor `T` so that
+  each diagonal block contains closely clustered eigenvalues from `T`.
+  Eigenvalues within diagonal blocks should also be well distributed
+  locally in order to preserve numerical stability.
+
+"""
+function block_pattern(δ::AbstractFloat, Λ::Vector{<:Complex})
+    n = length(Λ)
+    labels = zeros(Int, n)
+    nextlabel = 0
+    for i in 1:n
+        if labels[i] == 0
+            nextlabel += 1
+            labels[i] = nextlabel
+        end
+        for j in i+1:n
+            if abs(Λ[i] - Λ[j]) ≤ δ
+                if labels[j] == 0
+                    labels[j] = labels[i]
+                else
+                    # unify clusters labels[i] and labels[j]
+                    old = labels[j]
+                    labels[labels.==old] .= labels[i]
+                end
+            end
+        end
+    end
+    labels
+end
+
+function block_indices(q::Vector{Int})
+    unique_labels = unique(q)
+    blocks = Vector{UnitRange{Int}}()
+    for label in unique_labels
+        idxs = findall(q .== label)
+        push!(blocks, minimum(idxs):maximum(idxs))
+    end
+    blocks
+end
